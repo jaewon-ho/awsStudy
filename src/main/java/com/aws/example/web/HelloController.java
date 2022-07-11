9e0f2026870125acd7318e331c0b9f86d89b6f75
jaewon-ho <jwho5073@gmail.com>
2022-07-12 08:45:37 +0900

    Update Lombok

--- src/main/java/com/aws/example/web/HelloController.java
+++ src/main/java/com/aws/example/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.aws.example.web;
 
+import com.aws.example.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam(value = "name", required = false, defaultValue = "") String name,
+            @RequestParam(value = "amount", required = false, defaultValue = "0") int amount
+            ){
+
+        return new HelloResponseDto(name, amount);
     }
 }
